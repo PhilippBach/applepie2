066aaaec04d1d9a38cd68e20beb354d719cb3237
Philipp Bach <bach.philipp@outlook.com>
2021-07-07 10:23:06 +0200

    error url

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -3,10 +3,10 @@ test_that("get_packages() works", {
   expect_vector(pkgs, character())
 })
 
-#
-# test_that("get_packages() error", {
-#   vcr::skip_if_vcr_off()
-#   vcr::use_cassette("packages-error", {
-#     expect_error(get_packages("maelle"), "take off")
-#   })
-# })
+
+test_that("get_packages() error", {
+  vcr::skip_if_vcr_off()
+  vcr::use_cassette("packages-error", {
+    expect_error(get_packages("maelle"), "take off")
+  })
+})
