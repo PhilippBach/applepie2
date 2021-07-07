b11023d2c0ac74355575e4ee8d971be5da84762a
Philipp Bach <bach.philipp@outlook.com>
2021-07-07 10:21:47 +0200

    casette

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -2,3 +2,11 @@ test_that("get_packages() works", {
   pkgs <- get_packages("ropensci")
   expect_vector(pkgs, character())
 })
+
+#
+# test_that("get_packages() error", {
+#   vcr::skip_if_vcr_off()
+#   vcr::use_cassette("packages-error", {
+#     expect_error(get_packages("maelle"), "take off")
+#   })
+# })
